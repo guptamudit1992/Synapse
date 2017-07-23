63738ac30f60a321465d0d5844389a85e3654861
Mudit Gupta <mudit.gupta@caringmillions.com>
2017-07-23 22:09:53 +0530

    Feature Update - Homepage

---
+++ synapse/api/controllers/MockapiController.js
@@ -0,0 +1,11 @@
+/**
+ * MockapiController
+ *
+ * @description :: Server-side logic for managing mockapis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
